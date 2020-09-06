c32cc8f8466673c11016015af11c76530e82fc00
taylorkraft <taylorkraft16@gmail.com>
2020-09-06 00:11:32 -0600

    update portolio balance method

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,4 +3,22 @@ class Portfolio < ApplicationRecord
 
   # our portfolio must have a name and balance
   validates :name, :balance, presence: true
+
+  # When a trade is created we will call this method on a specific portfolio and update the balance
+  # We will pass it a trade argument, a trade has a trade_type of buy or sell & an amount
+  def update_balance(trade)
+    if trade.trade_type == 'buy'
+      self.balance = self.balance + trade.amount
+      self.save
+    elsif trade.trade_type == 'sell'
+
+      # If the balance of this portfolio is greater than or equal to the trade amount, that means we have enough currency to sell
+      if self.balance >= trade.amount
+        self.balance = self.balance - trade.amount
+        self.save
+      else 
+        return "Uh, oh! Looks like you don't have enough currency to sell."
+      end
+    end
+  end
 end
