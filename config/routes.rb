656f87f935d1c65eab36faefe2fc043f1f58ca4c
taylorkraft <taylorkraft16@gmail.com>
2020-09-05 23:24:40 -0600

    namespace controllers/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :trades
-  resources :portfolios
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :trades
+      resources :portfolios
+    end
+  end
 end
