33f83c1882dcb75704c12b98e8bdb4b663d6a06b
taylorkraft <taylorkraft16@gmail.com>
2020-09-06 00:24:34 -0600

    portfolio controller index route

--- app/controllers/api/v1/portfolios_controller.rb
+++ app/controllers/api/v1/portfolios_controller.rb
@@ -2,11 +2,12 @@ class Api::V1::PortfoliosController < ApplicationController
 
 
   def index
-
+    @portfolios = Portfolio.all
+    render json: @portfolios
   end
 
   def create
-
+  
   end
 
   def show
