cf163ec7e0f680bf4193000ebfd363f1bd71d70f
taylorkraft <taylorkraft16@gmail.com>
2020-09-06 00:56:46 -0600

    config routes refactor

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :trades
-      resources :portfolios
+      resources :portfolios do
+        resources :trades
+      end
     end
   end
 end
+
+# We no longer have access to /api/v1/trades
+# Our trade routes with associate with a specific portfolio
+# Ex:
+# Request -> /api/v1/portfolios/1/trades
+# Returns a list of trades for portfolio 1
