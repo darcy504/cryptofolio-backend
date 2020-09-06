4ba16fc56a5d20c990dae713cb84f9514eb26250
taylorkraft <taylorkraft16@gmail.com>
2020-09-06 00:34:04 -0600

    trade params private method

--- app/controllers/api/v1/trades_controller.rb
+++ app/controllers/api/v1/trades_controller.rb
@@ -14,6 +14,13 @@ class Api::V1::TradesController < ApplicationController
   end
 
   def destroy
-    
+
+  end
+
+  private
+
+  # Specifies the attributes a trade can be created with
+  def portfolio_params
+    params.require(:trade).permit(:portfolio_id, :trade_type, :amount, :currency_symbol, :date)
   end
 end
