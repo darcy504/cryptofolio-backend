12b4248513c9ac1bac495eb7c4f7dd2e39bbfb76
taylorkraft <taylorkraft16@gmail.com>
2020-09-05 23:19:52 -0600

    add validations to trade model

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -3,4 +3,7 @@ class Trade < ApplicationRecord
 
   # when form is submitted for a new trade, must enter an amount
   validates :amount, presence: true
+
+  # when someone creates a new trade and they give the trade_type attribute a value, that value must be included in this array
+  validates_inclusion_of :trade_type, :in => ['buy', 'sell']
 end
