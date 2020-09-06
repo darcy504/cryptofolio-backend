3430939ae2044c6c938d864dc54e1868f8800eb3
taylorkraft <taylorkraft16@gmail.com>
2020-09-05 23:10:13 -0600

    model validations

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,6 @@
 class Portfolio < ApplicationRecord
   has_many :trades
+
+  # our portfolio must have a name and balance
+  validates :name, :balance, presence: true
 end
--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,3 +1,6 @@
 class Trade < ApplicationRecord
   belongs_to :portfolio
+
+  # when form is submitted for a new trade, must enter an amount
+  validates :amount, presence: true
 end
