fdd0a643a0bd9fb3d5b2613c1ba479edcc9b6378
taylorkraft <taylorkraft16@gmail.com>
2020-09-06 00:19:12 -0600

    portfolio params private method

--- app/controllers/api/v1/portfolios_controller.rb
+++ app/controllers/api/v1/portfolios_controller.rb
@@ -16,4 +16,11 @@ class Api::V1::PortfoliosController < ApplicationController
   def destroy
 
   end
+
+  private
+
+  # Specifies the attributes a portfolio can be created with
+  def portfolio_params
+    params.require(:portfolio).permit(:name, :balance)
+  end
 end
